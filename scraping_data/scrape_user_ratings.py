91c21073308e5b978a789c3912462242be520cd1
Abigail Beasley <abigailbeasley98@gmail.com>
2023-10-11 13:14:58 -0700

    tested scroll function for headless browsing

---
+++ scraping_data/scrape_user_ratings.py
@@ -0,0 +1,141 @@
+'''
+Description: This script is intended pulls urls and ratings of the top reviews of a book.
+'''
+
+from bs4 import BeautifulSoup
+import selenium as sl
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains # to scroll to an element
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+def element_exists(xpath, driver):
+    '''
+    Returns whether or not a given element exists
+    '''
+    try:
+        driver.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+def load_reviews(n, url, wait_time: int = 10):
+    '''
+    This function loads reviews for a Goodreads book page
+    Inputs:
+    - n: the number of pages of reviews to load
+    - url: the URL of the desired Goodreads page for which reviews
+    - wait_time: specifies the number of seconds to wait for a page to load after an action
+    shoul be loaded from
+    Returns: the page source for the webpage after loading the desired
+    number of pages of reviews.
+    '''
+
+    second_load_button_xpath = '/html/body/div[1]/div[2]/main/div[1]/div[2]/div[4]/div[5]/div/button/span[2]'
+    first_load_button_xpath = '/html/body/div[1]/div[2]/main/div[1]/div[2]/div[3]/div/div[5]/div[4]/a/span[1]'
+    close_button_xpath = '/html/body/div[3]/div/div[1]/div/div/button'
+    whole_overlay_path = '/html/body/div[3]'
+    first_page_height_subtraction = 2500
+    headless_first_page_subtraction = 1900
+   
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(url)
+    driver.maximize_window()
+
+    time.sleep(wait_time)
+
+    # initial click
+    i = 1
+    while True and i < 2:
+        # intial scroll height
+        scroll_height = driver.execute_script('return document.body.scrollHeight') - headless_first_page_subtraction
+        try:
+            driver.execute_script("window.scrollTo(0, " + str(scroll_height) + ");")
+            driver.save_screenshot('headless_scroll.png')
+            print(scroll_height)
+            time.sleep(wait_time)
+            button = driver.find_element(By.XPATH, first_load_button_xpath)
+            print('Found the primary button!')
+            ActionChains(driver).move_to_element(button).click().perform()
+            print('Clicked the primary button')
+            time.sleep(wait_time)
+            i = i+1
+        except Exception as e:
+            if element_exists(whole_overlay_path, driver):
+                # click X button
+                print('Exited out of ad')
+                close_button = driver.find_element(By.XPATH, close_button_xpath)
+                ActionChains(driver).move_to_element(close_button).click().perform()
+            else:
+                print("Issue with first 'load more' click. See error message for more:")
+                driver.save_screenshot('error shot.png')
+                print(e)
+                break
+    
+        while True and i <= n:
+            scroll_height = driver.execute_script('return document.body.scrollHeight')
+            try:
+                driver.execute_script("window.scrollTo(0, " + str(scroll_height) + ");")
+                time.sleep(wait_time)
+                button = driver.find_element(By.XPATH, second_load_button_xpath)
+                print('Found the secondary button!')
+                ActionChains(driver).move_to_element(button).click().perform()
+                print('Clicked the secondary button')
+                time.sleep(wait_time)
+                i = i+1
+            except Exception as e:
+                if element_exists(whole_overlay_path, driver):
+                    # click X button
+                    print('Exited out of ad')
+                    close_button = driver.find_element(By.XPATH, close_button_xpath)
+                    ActionChains(driver).move_to_element(close_button).click().perform()
+                else:
+                    print('Unable to get to page ' + str(i) + ' of reviews.')
+                    driver.save_screenshot('error shot.png')
+                    print(e)
+                    break
+    time.sleep(wait_time)
+
+    return driver.page_source
+
+def grab_reveiwers_and_ratings(html):
+    '''
+    This function parses html to extract reveiewer info. It takes a page_source as an input and
+    It returns a pandas dataframe containing the url to the author's page as well as the
+    author's rating for each review.
+    '''
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    user_links = list()
+    user_ratings = list()
+
+    #list of lists
+    reviews = (soup.find('div', id='__next')
+            .find('div', class_='PageFrame PageFrame--siteHeaderBanner')
+            .find('main', class_='PageFrame__main BookReviewsPage')
+            .find('div', class_='BookReviewsPage__gridContainer')
+            .find('div', class_='BookReviewsPage__rightColumn')
+            .find('div', class_='ReviewsList').find_all(class_= 'ReviewCard'))
+    for review in reviews:
+        user_links.append(review.find('a', class_ = 'Avatar Avatar--medium', href = True)['href'])
+
+        user_ratings.append(review.find('span', class_ = 'RatingStars RatingStars__small')['aria-label'])
+    
+    data = pd.DataFrame({'user_links': user_links, 'user_ratings': user_ratings})
+
+    print(data.info())
+
+url = 'https://www.goodreads.com/en/book/show/60194162'
+
+load_reviews(10, url, 2)
